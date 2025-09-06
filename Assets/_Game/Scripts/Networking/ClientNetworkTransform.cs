ff00d6aa1d71e65d65293fc3ac3600d742fc5b41
Will <Williamwadbr@gmail.com>
2025-09-06 11:25:24 +0200

    Networking Foundation

---
+++ Assets/_Game/Scripts/Networking/ClientNetworkTransform.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Unity.Netcode.Components;
+
+public class ClientNetworkTransform : NetworkTransform
+{
+
+
+    protected override bool OnIsServerAuthoritative()
+    {
+        return false;
+    }
+}
